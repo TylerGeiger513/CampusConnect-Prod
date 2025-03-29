b6e06cb7c7111b901554da37ac66090d329139cf
Tyler <tygg513@outlook.com>
2025-03-29 15:43:50 -0400

    trying again - forgot helm install

--- deploy_scripts/install_deps.sh
+++ deploy_scripts/install_deps.sh
@@ -28,6 +28,12 @@ echo "📦 Installing kubectl..."
 curl -LO "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
 
+echo "📦 Installing Helm..."
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
+
 mkdir -p /local/logs
 chown -R ccuser:ccuser /local/logs
 
